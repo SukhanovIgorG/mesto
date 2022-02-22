d4a19d1da440556f5280aea5fbe9a6242e5cc78e
Igor Sukhanov <sukhanovgarik@yandex.ru>
2022-02-22 17:24:07 +0300

    hot fix подключить шрифт форматировать ЖС

--- script/index.js
+++ script/index.js
@@ -1,34 +1,37 @@
 let editButton = document.querySelector(".profile__edit-button"); // нажатие на редактирование профиля
-
 let popup = document.querySelector(".popup");
+let formElement = popup.querySelector(".popup__form"); // Находим форму в DOM
+let closButton = document.querySelector(".popup__close-button"); // нажатие на закрытие формы редактирования
+let nameInput = formElement.querySelector("#name"); // Находим поля формы в DOM
+let jobInput = formElement.querySelector("#sign"); // Воспользуйтесь инструментом .querySelector()
+
+let profile = document.querySelector(".profile");
+let profileName = profile.querySelector(".profile__name"); // Выберите элементы, куда должны быть вставлены значения полей
+let profileSign = profile.querySelector(".profile__sign");
+
 function popupVisible() {
   popup.classList.add("popup_visible");
-}
-editButton.addEventListener("click", popupVisible);
 
-let closButton = document.querySelector(".popup__close-button"); // нажатие на закрытие формы редактирования
+  popup.querySelector("#name").value = profileName.textContent;
+  popup.querySelector("#sign").value = profileSign.textContent;
+}
+
 function popupInvisible() {
   popup.classList.remove("popup_visible");
 }
-closButton.addEventListener("click", popupInvisible);
-
-let formElement = document.querySelector(".popup__form"); // Находим форму в DOM
-
-let nameInput = formElement.querySelector("#name"); // Находим поля формы в DOM
-let jobInput = formElement.querySelector("#sign"); // Воспользуйтесь инструментом .querySelector()
 
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
-  let newName = popup.querySelector("#name").value;   // Получите значение полей jobInput и nameInput из свойства value
+  console.log(popup.querySelector("#name").value);
+
+  let newName = popup.querySelector("#name").value; // Получите значение полей jobInput и nameInput из свойства value
   let newSign = popup.querySelector("#sign").value;
 
-  let profile = document.querySelector(".profile");
-  let profileName = profile.querySelector(".profile__name");   // Выберите элементы, куда должны быть вставлены значения полей
-  let profileSign = profile.querySelector(".profile__sign");
-
-  profileName.textContent = newName;   // Вставьте новые значения с помощью textContent
+  profileName.textContent = newName; // Вставьте новые значения с помощью textContent
   profileSign.textContent = newSign;
 }
 
+editButton.addEventListener("click", popupVisible);
+closButton.addEventListener("click", popupInvisible);
 formElement.addEventListener("submit", formSubmitHandler); // он будет следить за событием “submit” - «отправка»
